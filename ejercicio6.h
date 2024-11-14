1fb571bcb45655a366fbe844e26199c0c6b31577
e-peisinov <eliaspeisinovazquez@gmail.com>
2024-11-14 01:25:24 -0300

    ejercicio6

---
+++ ejercicio6.h
@@ -0,0 +1,15 @@
+#ifndef ejercicio6_h
+#define ejercicio6_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "listas.h"
+#include "tabla_hash.h"
+#include "tipo_elemento.h"
+#include "validacion.h"
+
+void CargarDatos(TablaHash Vacunados);
+void BuscarVacunadosPorFecha(TablaHash Vacunados);
+
+#endif // ejercicio6_h
